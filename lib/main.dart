3a1ecfe50a264d74bbd2c3bb755ab6e93cbf90b3
Filipe Nogueira Silveira <86435640+fnosilveira@users.noreply.github.com>
2022-07-31 23:29:06 -0300

    First commit Flutter App Bank

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        body: ListaTransferencias(),
+        appBar: AppBar(
+          title: Text('Transfers'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add),
+        ),
+      ),
+    ));
+
+class ListaTransferencias extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Column(children: <Widget>[
+      ItemTransferencia(Transferencia(100.0, 1)),
+      ItemTransferencia(Transferencia(200.0, 2)),
+      ItemTransferencia(Transferencia(300.0, 3)),
+    ]);
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta.toString()),
+      ),
+    );
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
+}
